004a639fd7ba161de8392ce6425a34ced22daab7
Julian A Avar C <campopianojulian@johndeere.com>
2022-02-17 16:58:50 -0600

    add example routing

--- src/main/java/com/example/bandalee/BandaleeApplication.java
+++ src/main/java/com/example/bandalee/BandaleeApplication.java
@@ -2,12 +2,20 @@ package com.example.bandalee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BandaleeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BandaleeApplication.class, args);
 	}
+
+	@GetMapping
+	public String hello() {
+		return "hello";
+	} 
 
 }
