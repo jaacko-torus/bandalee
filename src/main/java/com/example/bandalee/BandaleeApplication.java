d5d2904ee7c2dcde3ca7c736b4d2536554e95762
Julian A Avar C <campopianojulian@johndeere.com>
2022-02-17 16:41:43 -0600

    init

---
+++ src/main/java/com/example/bandalee/BandaleeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bandalee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandaleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandaleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bandalee/BandaleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bandalee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandaleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
